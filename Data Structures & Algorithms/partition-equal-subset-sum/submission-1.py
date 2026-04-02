84c59ed93d66613f02f23d47b64eb70512dabb63
Ntwari Bruce <107446340+ntwari-bruce@users.noreply.github.com>
2026-04-02 13:25:01 -0700

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # if the total sum is odd, impossible to split with integers
+        # the idea is that i dp we'll fill in take it or leave it
+        # in dp, we are adding all the subsets sum reachable up to that number
+        # say we're {0, 5}
+        if sum(nums) % 2:
+            return False
+        
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+
+        for i in range(len(nums) - 1, -1, -1):
+            nextDp = set()
+            for t in dp:
+                nextDp.add(t + nums[i])
+                nextDp.add(t)
+            
+            dp = nextDp
+        
+        return True if target in dp else False
+
+        # time complexit of O(n * target)
+        # space is O(target)
+
+
+        
