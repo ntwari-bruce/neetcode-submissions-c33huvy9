1f11f2e57cb3f9f4de16c7197499e7a56fbd3a4e
Ntwari Bruce <107446340+ntwari-bruce@users.noreply.github.com>
2026-04-28 16:40:23 -0700

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # when thinking through these dynamic programing questions,
+        # just evaluate the first few initial steps and build upon them
+        # we optimise the solution by caching
+        cache = [-1] * len(nums)
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+
+            if cache[i] != -1:
+                return cache[i]
+            # we can choose to rob the first + third, or rob the second
+            cache[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            return cache[i]
+        
+        return dfs(0)
+
+        # after optimization
+        # time complexity is O(n)
+        # time complexity is O(n) # the recursion stack
+
+
