03e053a76f3d343b74314d6f693bb28599e02cb5
Ntwari Bruce <107446340+ntwari-bruce@users.noreply.github.com>
2026-09-06 21:06:26 -0500

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        cache = {}
+
+        def dfs(amount):
+
+            if amount == 0:
+                return 0
+
+            # we start by setting the result for the highest possible number
+            res = float("inf")
+
+            if amount in cache:
+                return cache[amount]
+
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1 + dfs(amount - coin))
+                cache[amount] = res
+            
+
+            return res
+        
+        final = dfs(amount)
+
+        return -1 if final == float("inf") else final
