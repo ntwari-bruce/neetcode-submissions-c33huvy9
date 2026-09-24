90ed83aede2d81a7a4874b8ad87352a26820fca8
Ntwari Bruce <107446340+ntwari-bruce@users.noreply.github.com>
2026-09-24 17:12:35 -0500

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # we start from the corner grid[0][0]
+        # we take paths from the right,and move down
+        # then have their sum
+        cache = {}
+
+        def dfs(i, j):
+
+            if (i, j) in cache:
+                return cache[(i, j)]
+            # base case
+            if i == (m - 1) and j == (n - 1):
+                return 1
+            
+            # return we get out of bounds, return 0
+            if i >= m or j >= n:
+                return 0
+            
+            # move right
+            right = dfs(i, j + 1)
+            # move down
+            down = dfs(i + 1, j)
+
+            cache[(i, j)] = right + down
+            return right + down
+        
+        return dfs(0, 0)
+
+        # on each cell we make two decision which grows with the number of rows and columns
+        # The recursion depth is the size of O(m + n)
